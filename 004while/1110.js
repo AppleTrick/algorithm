78e98ea8763935357731236cb1eb0088c88ced79
Changhee Park <arrans9591@gmail.com>
2021-02-07 00:53:25 +0900

    baekjoon js

---
+++ 004while/1110.js
@@ -0,0 +1,17 @@
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+// const A = +(input[0]);
+
+const A = 26;
+
+const one = parseInt(A / 10);
+const two = A % 10;
+const count = 0;
+
+console.log(one)
+console.log(two)
+
+while (1) {
+    count ++;
+    
+}
