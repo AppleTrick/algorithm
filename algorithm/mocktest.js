63460e92acc1333505f72c49e51c57c2f7a0595a
Changhee Park <arrans9591@gmail.com>
2021-02-14 01:01:26 +0900

    mocktest

---
+++ algorithm/mocktest.js
@@ -0,0 +1,12 @@
+function solution(answers) {
+
+
+    var answer = [];
+
+    var a = [1,2,3,4,5];
+    var b = [2,1,2,3,2,4,2,5];
+    var c = [3,3,1,1,2,2,4,4,5,5];
+
+    
+    return answer;
+}
