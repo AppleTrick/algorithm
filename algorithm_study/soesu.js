6222a7e80ef370d27b5998460d150284c5d12a75
AppleTrick <arrans9591@gmail.com>
2021-05-08 00:52:50 +0900

    소수찾기

---
+++ algorithm_study/soesu.js
@@ -0,0 +1,49 @@
+
+function solution(numbers) {
+
+    // 문자열 배열을 문자로 만듬
+    const arr = numbers.split('');
+
+    // Set 으로 중복 제거
+    const answer = new Set();
+
+    // 배열 재정렬
+    research(arr, '', answer);
+
+    //console.log(answer);
+    return answer.size
+
+}
+
+function research(arr , preNum, set){
+    for(let i of arr.keys()){
+        let setNum = parseInt(preNum + arr[i]);
+        decimal(setNum,set);
+        let temp = [...arr];
+        temp.splice(i,1);
+        research(temp, setNum, set);
+    }
+
+}
+
+// 소수 판별 알고리즘
+function decimal(val , set){
+    for(let i = 2; i<=val; i++){
+        if(val % i === 0){
+            if(i == val){
+                set.add(val); 
+            }else{
+                break;
+            }
+        }
+    }
+}
+
+let numbers = "17";
+solution(numbers);
+
+// let arr = [1,2,3];
+// arr = [...arr];
+// console.log(arr);
+// arr.splice(1,1);
+// console.log(arr);
