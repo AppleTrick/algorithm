bdf66f7c9451f6ea8141580f2d618d49c337b681
AppleTrick <arrans9591@gmail.com>
2021-05-16 22:18:36 +0900

    commit

--- algorithm_study/bestWorstLotto.js
+++ algorithm_study/bestWorstLotto.js
@@ -15,7 +15,15 @@ function solution(lottos, win_nums) {
         }
     });
 
-    console.log(count, bestCount);
+    if(count == 0 && bestCount == 0){
+        return [6,6]
+    }
+
+    if(count == 0){
+        return [7-bestCount,6];
+    }
+
+    return [7-(count+bestCount),7-count];
     
 }
 
