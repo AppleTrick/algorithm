bd0e19ae0f32bf947370a52dc7c9225e1b930634
Changhee Park <arrans9591@gmail.com>
2021-05-30 19:45:28 +0900

    carpet

---
+++ algorithm_study/carpet.js
@@ -0,0 +1,26 @@
+function solution(brown, yellow) {
+    let mul = yellow
+    let plus = (brown-4) / 2;
+    let num = plus
+    let resultA = 0;
+    let resultB = 0;
+
+    //console.log(mul,plus,num,resultA, resultB);
+    
+    for (let i = 0; i <= num; i++) {
+        if((num * i) === mul){
+            resultA = num;
+            resultB = i;
+            break
+        }
+        num--;   
+    }
+
+  return [resultA+2 ,resultB+2];
+
+}
+
+const brown = 8;
+const yellow = 1;
+
+solution(brown,yellow);
