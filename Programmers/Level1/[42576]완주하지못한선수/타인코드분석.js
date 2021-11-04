0c9401f2fd8e4c8435541404806ac83f2a2dc22d
AppleTrick <arrans9591@gmail.com>
2021-11-04 18:47:24 +0900

    타인 코드 분석

--- Programmers/Level1/[42576]완주하지못한선수/타인코드분석.js
+++ Programmers/Level1/[42576]완주하지못한선수/타인코드분석.js
@@ -23,15 +23,29 @@
 // arr["park"] = (arr["park"] | 0) + 1;
 // console.log(arr["park"]);
 
-var solution = (participant,completion) => {
-     completion.map(name => completion[name] = (completion[name]|0)+1); 
-     console.log(completion);
-     console.log(participant.map((name) => !completion[name]--));
-    //  return participant.find((name) => !completion[name]--); 
-};
+// var solution = (participant,completion) => {
+//      completion.map(name => completion[name] = (completion[name]|0)+1); 
+//      console.log(completion);
+//      console.log(participant.map((name) => !completion[name]--));
+//     //  return participant.find((name) => !completion[name]--); 
+// };
+
+
+//찢었다..
+
+
+function solution(participant, completion) {
+    var dic = completion.reduce((obj, t)=> (obj[t]= obj[t] ? obj[t]+1 : 1 , obj) ,{});
+    console.log(dic);
+
+    return participant.find(t=> {
+        if(dic[t])
+            dic[t] = dic[t]-1;
+        else 
+            return true;
+    });
+}
 
 let participant = ["mislav", "stanko", "mislav", "ana"];
 let completion = ["stanko", "ana", "mislav"];
 console.log(solution(participant,completion));
-
-//
