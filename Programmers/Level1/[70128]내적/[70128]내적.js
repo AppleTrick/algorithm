27adf2e7211b75cdee44dc1d37b3a47f300a46bd
AppleTrick <arrans9591@gmail.com>
2021-11-04 17:09:01 +0900

    내적 reduce로 풀어본것

--- Programmers/Level1/[70128]내적/[70128]내적.js
+++ Programmers/Level1/[70128]내적/[70128]내적.js
@@ -9,3 +9,12 @@ function solution(a, b) {
 let a = [1,2,3,4];
 let b = [-3,-1,0,2];
 console.log(solution(a,b));
+
+// feedback1
+// reduce로 풀어볼것!;
+
+function solution1(a, b) {
+    return a.reduce((acc, cur, idx) => acc = acc + cur*b[idx], 0);
+}
+
+console.log(solution1(a,b));
