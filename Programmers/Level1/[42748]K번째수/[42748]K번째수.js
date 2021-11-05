4888a1e1299baa58dc7dbac12ae1a82828d12f4a
AppleTrick <arrans9591@gmail.com>
2021-11-05 20:38:38 +0900

    k번째 수

--- Programmers/Level1/[42748]K번째수/[42748]K번째수.js
+++ Programmers/Level1/[42748]K번째수/[42748]K번째수.js
@@ -14,3 +14,20 @@ const array = [1, 5, 2, 6, 3, 7, 4];
 const commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]];
 
 console.log(solution(array, commands));
+
+
+// feedback1
+
+/*
+
+        function solution(array, commands) {
+            return commands.map(command => {
+                const [sPosition, ePosition, position] = command
+                const newArray = array
+                    .filter((value, fIndex) => fIndex >= sPosition - 1 && fIndex <= ePosition - 1)
+                    .sort((a,b) => a - b)    
+
+                return newArray[position - 1]
+            })
+        }
+*/
