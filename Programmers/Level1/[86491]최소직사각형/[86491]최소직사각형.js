42007668a6322bd6e590b2921380061cca010e76
AppleTrick <arrans9591@gmail.com>
2021-11-06 17:35:16 +0900

    최소 직사각형

---
+++ Programmers/Level1/[86491]최소직사각형/[86491]최소직사각형.js
@@ -0,0 +1,27 @@
+function solution(sizes) {
+    let a = 0;
+    let b = 0;
+
+    sizes.map((element) => {
+        element.sort((a,b) => {return a-b})
+    });
+
+    // console.log(sizes);
+
+    sizes.forEach(element => {
+        a = a > element[0] ? a : element[0];
+        b = b > element[1] ? b : element[1];
+    });
+    // console.log(a,b);
+    return a * b;
+    
+}
+
+let sizes = [[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]
+console.log(solution(sizes));
+
+// feedback 1
+
+/*  
+    구조 분해할당을 잘 쓰기
+*/
