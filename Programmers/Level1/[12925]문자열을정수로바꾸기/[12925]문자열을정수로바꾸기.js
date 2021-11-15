9c90f1433e536c387065e167de9346fe23d3a3a5
AppleTrick <arrans9591@gmail.com>
2021-11-15 18:52:14 +0900

    문자열을 정수로 바꾸기

---
+++ Programmers/Level1/[12925]문자열을정수로바꾸기/[12925]문자열을정수로바꾸기.js
@@ -0,0 +1,14 @@
+function solution(s) {
+    let answer = Number(s);
+    // return answer;
+    // return s/1
+    // return   + s
+}
+
+let s = "-1234";
+console.log(solution(s));
+
+/*
+feedback 자바스크립트의 동적 환경을 사용한 예시들이 있다.
+
+*/
