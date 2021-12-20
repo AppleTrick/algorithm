7efc560ac49b628895a4316f97a58ccd0ef4dd78
AppleTrick <arrans9591@gmail.com>
2021-12-20 19:45:16 +0900

    짝지어제거하기

---
+++ Programmers/Level2/[12973]짝지어제거하기/[12973]짝지어제거하기.js
@@ -0,0 +1,26 @@
+function solution(s)
+{
+    s = s.split('');
+
+    let isTrue = true;
+
+    while(isTrue && s.length > 0){
+        for (let i = 1; i < s.length; i++) {
+            if(s[i]===s[i-1]){
+                s.splice(i-1,2)
+                isTrue = true;
+                break;
+            }else{
+                isTrue = false;
+                continue;
+            }
+        }
+    }
+
+    console.log(s);
+    
+    return s.length > 0 ? 0 : 1;
+}
+
+const s = "baabaa"
+console.log(solution(s));
