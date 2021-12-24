5b833a2aead1f497a630e9c87334acb44719d2fe
AppleTrick <arrans9591@gmail.com>
2021-12-24 17:23:42 +0900

    hindex

--- Programmers/Level2/[42747]H-index/[42747]H-index copy.js
+++ Programmers/Level2/[42747]H-index/[42747]H-index copy.js
@@ -1,12 +1,10 @@
 function solution(citations) {
 
-    let n = citations.length;
-
     let h = 0;
 
     while(true){
         let value = citations.filter((element) => element >= h).length; // h이상으로 인용된 횟수
-        console.log("h" , h,"value",value);
+        // console.log("h" , h,"value",value);
         if(value >= h){
 
         }else{
