f4ca0d42c013e73fd48fa8c997e81f73f5f77c82
ChangHee Park <arrans9591@gmail.com>
2022-01-10 15:17:39 +0900

    빛의 경로 사이클

---
+++ Programmers/Level2/[86052]빛의경로사이클 (못푼 문제임 다시 풀기)/[86052]빛의경로사이클.js
@@ -0,0 +1,65 @@
+function solution(grid) {
+
+
+    const result = []; 
+    const cycle = grid.map((row) => row.split('').map((_) => new Array(4).fill(true)));
+    const direct = [
+        [1, 0], // 아래
+        [0, 1], // 우
+        [-1, 0], // 위
+        [0, -1] // 좌
+    ];
+
+    cycle.forEach((row, rdx) => { 
+        // rdx 가로 인덱스
+        row.forEach((col, cdx) => { 
+            // cdx 세로 인덱스
+            col.forEach((route, idx) => { 
+                // idx 요소 인덱스
+                if (route) { 
+                    result.push(checkCycle(rdx, cdx, idx)) 
+                } 
+            }); 
+        }); 
+    });
+
+    function checkCycle(rdx, cdx, idx){
+        let result = 0;
+
+        while(true){
+            // 종료조건
+            if (!cycle[rdx][cdx][idx]){
+                break;
+            } 
+
+            cycle[rdx][cdx][idx] = false;
+            result++;
+
+            rdx += direct[idx][0];
+            cdx += direct[idx][1];
+
+            if (rdx < 0) rdx = cycle.length - 1; 
+            if (rdx >= cycle.length) rdx = 0; 
+            if (cdx < 0) cdx = cycle[0].length - 1; 
+            if (cdx >= cycle[0].length) cdx = 0; 
+
+
+            if (grid[rdx][cdx] === "L") idx = [1, 2, 3, 0][idx]; 
+            if (grid[rdx][cdx] === "R") idx = [3, 0, 1, 2][idx];
+
+
+        }
+
+        return result
+
+
+    }
+
+    
+    return result.sort((a, b) => a - b);
+
+}
+
+const grid = ["SL","LR"];
+
+console.log(solution(grid));
---
+++ Programmers/Level2/[86052]빛의경로사이클 (못푼 문제임 다시 풀기)/test.js
@@ -0,0 +1,5 @@
+let a = 1
+
+let b = [100,200,300,400][a];
+
+console.log(b);
