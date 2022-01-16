c4f8c2036a278c97ebb374e6ed17a292e517ec43
ChangHee Park <arrans9591@gmail.com>
2022-01-16 15:52:04 +0900

    k진수 소수 구하기 다른풀이 코드 해석

---
+++ Programmers/Level2/[92335]k진수소수구하기/[92335]k진수소수구하기_다른풀이_해석.js
@@ -0,0 +1,21 @@
+function isPrime(num){
+    if(!num || num===1) return false;
+    for(let i=2; i<=+Math.sqrt(num); i++){
+        if(num%i===0) return false;
+    }
+    return true;
+}
+
+function solution(n, k) {    
+    // k진법으로 나눈 후 split
+    const candidates = n.toString(k).split('0');
+    // 소수 개수 세기
+
+    console.log(candidates.filter(v=>isPrime(v)));
+    return candidates.filter(v=>isPrime(+v)).length;
+}
+
+const n = 110011;
+const k = 10;
+
+console.log(solution(n,k));
